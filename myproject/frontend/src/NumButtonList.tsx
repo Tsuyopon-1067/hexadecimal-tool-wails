de860ebed524b75571356cad3b10309f2f24fef4
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-04-24 13:14:36 +0900

    fix: button label

--- myproject/frontend/src/NumButtonList.tsx
+++ myproject/frontend/src/NumButtonList.tsx
@@ -12,14 +12,14 @@ function NumButtonList() {
             <NumButton num={5} text="0x5: 0b 0101" />
             <NumButton num={6} text="0x6: 0b 0110" />
             <NumButton num={7} text="0x7: 0b 0111" />
-            <NumButton num={8} text="0x8: 0b 0000" />
-            <NumButton num={9} text="0x9: 0b 0001" />
-            <NumButton num={10} text="0xA: 0b 0010" />
-            <NumButton num={11} text="0xB: 0b 0011" />
-            <NumButton num={12} text="0xC: 0b 0100" />
-            <NumButton num={13} text="0xD: 0b 0101" />
-            <NumButton num={14} text="0xE: 0b 0110" />
-            <NumButton num={15} text="0xF: 0b 0111" />
+            <NumButton num={8} text="0x8: 0b 1000" />
+            <NumButton num={9} text="0x9: 0b 1001" />
+            <NumButton num={10} text="0xA: 0b 1010" />
+            <NumButton num={11} text="0xB: 0b 1011" />
+            <NumButton num={12} text="0xC: 0b 1100" />
+            <NumButton num={13} text="0xD: 0b 1101" />
+            <NumButton num={14} text="0xE: 0b 1110" />
+            <NumButton num={15} text="0xF: 0b 1111" />
         </div>
     );
 }
