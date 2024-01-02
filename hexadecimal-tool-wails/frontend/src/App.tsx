6bb5402da4c4cdddecc3536d2737bfb332636fd6
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-01-02 23:49:09 +0900

    :lipstick: layout: css grid in App.tsx

--- hexadecimal-tool-wails/frontend/src/App.tsx
+++ hexadecimal-tool-wails/frontend/src/App.tsx
@@ -1,28 +1,67 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { useState } from "react";
+import logo from "./assets/images/logo-universal.png";
+import "./App.css";
+import { Greet } from "../wailsjs/go/main/App";
+import NumButtonList from "./compoents/numButtonList";
+import NumberDisplayList from "./compoents/numberDisplayList";
+import styles from "./App.module.css";
+import { Push, GetValueData } from "../wailsjs/go/main/Complement";
+import { ValueData, defaultValueData } from "./types";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
+    const [resultText, setResultText] = useState(
+        "Please enter your name below 👇"
+    );
+    const [name, setName] = useState("");
     const updateName = (e: any) => setName(e.target.value);
     const updateResultText = (result: string) => setResultText(result);
+    const [values, setValues] = useState<ValueData>();
 
     function greet() {
         Greet(name).then(updateResultText);
     }
+    const numberButtonClick = (key: number) => {
+        Push(key).then();
+    };
+    const renewValues = () => {
+        GetValueData().then((data) => setValues(data));
+    };
 
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
+            <div className={styles.mainDiv}>
+                <div className={styles.display}>
+                    <NumberDisplayList
+                        title="数値"
+                        data={values?.value || defaultValueData.value}
+                    />
+                    <NumberDisplayList
+                        title="補数"
+                        data={values?.value || defaultValueData.value}
+                    />
+                </div>
+                <div className={styles.buttonList}>
+                    <NumButtonList onClick={numberButtonClick} />
+                </div>
+            </div>
+            <div id="result" className="result">
+                {resultText}
+            </div>
             <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
+                <input
+                    id="name"
+                    className="input"
+                    onChange={updateName}
+                    autoComplete="off"
+                    name="input"
+                    type="text"
+                />
+                <button className="btn" onClick={greet}>
+                    Greet
+                </button>
             </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
