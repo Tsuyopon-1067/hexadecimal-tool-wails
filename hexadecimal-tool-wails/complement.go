89183c170874c1a4c527da8c5d9c3d1badf4a122
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-01-02 23:52:07 +0900

    :sparkles: feat: complement calculate

---
+++ hexadecimal-tool-wails/complement.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type ValueData struct {
+	value HexDecBin
+	complementValue HexDecBin
+}
+
+type HexDecBin struct {
+	hex string
+	dec string
+	bin string
+}
+
+var stack ComplementStack
+
+type Complement struct {
+	ctx context.Context
+}
+
+func NewComplement() *Complement {
+	stack = ComplementStack{}
+	return &Complement{}
+}
+
+func (c *Complement) startup(ctx context.Context) {
+	c.ctx = ctx
+}
+
+func (c *Complement) Push(v int) {
+	if (stack.IsFull()) {
+		fmt.Println("error: stack is full.")
+		return
+	}
+	stack.Push(v)
+}
+
+func (c *Complement) Pop() {
+	if (stack.IsEmpty()) {
+		fmt.Println("error: stack is empty.")
+		return
+	}
+	stack.Pop()
+}
+
+func (c *Complement) GetValueData() ValueData {
+	hex, dec, bin := stack.ToHexDecBinString()
+	cHex, cDec, cBin := stack.ToHexDecBinComplementString()
+	normal := HexDecBin{hex, dec, bin}
+	complement := HexDecBin{cHex, cDec, cBin}
+	return ValueData{normal, complement}
+}
