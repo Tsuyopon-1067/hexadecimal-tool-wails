0615917a7a198c6136d7eba0b72843d61a9c7257
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-01-03 18:17:59 +0900

    :bug: fix: prevent overflow
    Prevents overflow during complement calculation.

--- hexadecimal-tool-wails/complementStack.go
+++ hexadecimal-tool-wails/complementStack.go
@@ -47,38 +47,76 @@ func (s *ComplementStack) Delete() {
 
 func (s *ComplementStack) ToHexDecBinString() (string, string, string) {
 	int64Value := s.getInt64Value()
-	intValue := int(int64Value)
 
 	hexDigit := s.stack.Size()
 	binDigit := hexDigit * 4
-	return getHexDecBinString(intValue, hexDigit, binDigit)
+	return getHexDecBinString(int64Value, hexDigit, binDigit)
 }
 
 func (s *ComplementStack) ToHexDecBinComplementString() (string, string, string) {
-	int64Value := s.getInt64Value()
-	var maxInt int64 = 1
-	for _, _ = range s.stack.data {
-		maxInt *= 16
-	}
-	complementValue := maxInt - int64Value
-	intValue := int(complementValue)
+	array := s.getBinArray()
+	cArray := s.convertBinArrayToComplementArray(array)
+	int64Value := s.converBinArraytToUint64(cArray)
 
 	hexDigit := s.stack.Size()
 	binDigit := hexDigit * 4
-	return getHexDecBinString(intValue, hexDigit, binDigit)
+	return getHexDecBinString(int64Value, hexDigit, binDigit)
 }
 
-func (s *ComplementStack) getInt64Value() int64 {
+func (s *ComplementStack) getInt64Value() uint64 {
 	var hexString string
 	for _, v := range s.stack.data {
 		hexString += fmt.Sprintf("%X", v)
 	}
 
-	res, _ := strconv.ParseInt(hexString, 16, 64)
+	res, _ := strconv.ParseUint(hexString, 16, 64)
 	return res
 }
 
-func getHexDecBinString(value int, hexDigit int, binDigit int) (string, string, string) {
+func (s *ComplementStack) getBinArray() []int {
+	int64Value := s.getInt64Value()
+
+	var res []int
+	for i := 0; i < s.Size()*4; i++ {
+		if (int64Value % 2 == 0) {
+			res = append(res, 0)
+		} else {
+			res = append(res, 1)
+		}
+		int64Value /= 2
+	}
+	return res
+}
+
+func (s *ComplementStack) convertBinArrayToComplementArray(array []int) []int {
+	var res []int
+	for i, _ := range array {
+		res = append(res, 1 - array[i])
+	}
+
+	carry := 1
+	for i := 0; i < stack.Size(); i++ {
+		res[i] = carry + res[i]
+		if (res[i] == 2) {
+			res[i] = 0
+			carry = 1
+		} else {
+			carry = 0
+		}
+	}
+	return res
+}
+
+func (s *ComplementStack) converBinArraytToUint64(array []int) uint64 {
+	var res uint64 = 0
+	for i := stack.Size()*4-1; i >= 0; i-- {
+		res *= 2
+		res += uint64(array[i])
+	}
+	return res
+}
+
+func getHexDecBinString(value uint64, hexDigit int, binDigit int) (string, string, string) {
 	hexString := fmt.Sprintf("%016X", value)
 	decString := fmt.Sprintf("%d", value)
 	binString := fmt.Sprintf("%064b", value)
